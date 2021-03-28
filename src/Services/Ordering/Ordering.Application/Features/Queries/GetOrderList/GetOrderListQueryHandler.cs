5d041533bbbdb781fdfc56a365bb597c248c1831
RhadooPopescu <radu.popesku@gmail.com>
2021-03-28 20:38:30 +0100

    orderingAPI implementation

---
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrderList/GetOrderListQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Queries.GetOrderList
+{
+    public class GetOrderListQueryHandler 
+    {
+        
+    }
+}
