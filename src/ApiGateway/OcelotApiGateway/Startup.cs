b880010056a8447139c964bec4e417fbaae1a41b
RhadooPopescu <radu.popesku@gmail.com>
2021-04-07 18:49:12 +0100

    OcelotGateway deployed with docker

--- src/ApiGateway/OcelotApiGateway/Startup.cs
+++ src/ApiGateway/OcelotApiGateway/Startup.cs
@@ -5,10 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ocelot.Cache.CacheManager;
 
 namespace OcelotApiGateway
 {
@@ -19,7 +16,8 @@ namespace OcelotApiGateway
         public void ConfigureServices(IServiceCollection services)
         {
             //Ocelot configuration
-            services.AddOcelot();
+            services.AddOcelot()
+                    .AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
