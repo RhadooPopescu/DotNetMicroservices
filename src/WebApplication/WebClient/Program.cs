a93ad981ea1663776c2100d8dd5d4e067cd98227
RhadooPopescu <radu.popesku@gmail.com>
2021-05-14 01:00:16 +0100

    testing

--- src/WebApplication/WebClient/Program.cs
+++ src/WebApplication/WebClient/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 
-namespace AspnetRunBasics
+namespace WebClient
 {
     public class Program
     {
--- src/WebApplication/WebClient/Startup.cs
+++ src/WebApplication/WebClient/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using WebClient.Services;
 
-namespace AspnetRunBasics
+namespace WebClient
 {
     public class Startup
     {
@@ -42,7 +42,6 @@ namespace AspnetRunBasics
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
